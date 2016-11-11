7cfb74f45d1de705864f20cd66bbcddc31672944
herick024 <halorekiem024@gmail.com>
2016-11-11 16:05:43 -0600

    Actualiza archivos a las 4:06

---
+++ app.py
@@ -0,0 +1,25 @@
+import web
+
+render = web.template.render('views/')
+
+urls = (
+     '/','index',
+    '/about','about', 
+    '/data', 'data'
+)
+
+class index:
+     def GET(self):
+         return render.index()
+class about:
+    def GET(self):
+        return about
+class data:
+    def GET(self)
+        return.render.data()
+    
+
+if __name__ == '__main__':
+    app = web.application(urls,globals())
+    web.config.debug = True
+    app.run()
